ca740d048a02456169f8cd8dce835bb88816a36a
saurav2502 <sauravstarsk@gmail.com>
2019-02-17 10:44:29 +0530

    git

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/Todos">todo</router-link>
+        </li>
+      </ul>
+    </nav>
+    <HelloWorld/>
+    <Todos/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from "./components/Todos";
+//import HelloWorld from "./components/HelloWorld";
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    Todos
+    //HelloWorld
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "todo1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "todo2",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "todo3",
+          completed: false
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
-</style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+nav li {
+  color: #07c;
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style> 
---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <a href="/Todo" target="_blank">hello</a>
+    <br>
+    <p id="para1">hello world!</p>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+
+<style  scoped>
+#para1 {
+  display: initial;
+  color: aqua;
+  background-color: azure;
+}
+</style>
